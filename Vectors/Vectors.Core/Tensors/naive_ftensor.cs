c322fa67e21d00b98fa091eb777c62f73f548240
Lewis Edwards <lewis@9thcircle.net>
2022-03-20 14:17:41 +1100

    Initial commit of class structures

---
+++ Vectors/Vectors.Core/Tensors/naive_ftensor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vectors.Core.Tensors
+{
+    public class naive_ftensor : ftensor
+    {
+    }
+}
