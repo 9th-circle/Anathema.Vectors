f5dd40a86548f1b629f174bc039e1a25a0f09923
Lewis Edwards <lewis@9thcircle.net>
2023-10-05 16:37:35 +1100

    Finished gutting itvec4 impelementation details

--- Vectors/Vectors.Interfaces/itvec4.cs
+++ Vectors/Vectors.Interfaces/itvec4.cs
@@ -66,87 +66,33 @@ namespace Vectors.Interfaces
 
         public itvec4<T> yxzw
         {
-            get
-            {
-                return new tvec4<T>(y, x, z, w);
-            }
-            set
-            {
-                y = value.x;
-                x = value.y;
-                z = value.z;
-                w = value.w;
-            }
+            get;
+            set;
         }
         public itvec4<T> yxwz
         {
-            get
-            {
-                return new tvec4<T>(y, x, w, z);
-            }
-            set
-            {
-                y = value.x;
-                x = value.y;
-                w = value.z;
-                z = value.w;
-            }
+            get;
+            set;
         }
         public itvec4<T> yzxw
         {
-            get
-            {
-                return new tvec4<T>(y, z, x, w);
-            }
-            set
-            {
-                y = value.x;
-                z = value.y;
-                x = value.z;
-                w = value.w;
-            }
+            get;
+            set;
         }
         public itvec4<T> yzwx
         {
-            get
-            {
-                return new tvec4<T>(y, z, w, x);
-            }
-            set
-            {
-                y = value.x;
-                z = value.y;
-                w = value.z;
-                x = value.w;
-            }
+            get;
+            set;
         }
         public itvec4<T> ywzx
         {
-            get
-            {
-                return new tvec4<T>(y, w, z, x);
-            }
-            set
-            {
-                y = value.x;
-                w = value.y;
-                z = value.z;
-                x = value.w;
-            }
+            get;
+            set;
         }
         public itvec4<T> ywxz
         {
-            get
-            {
-                return new tvec4<T>(y, w, x, z);
-            }
-            set
-            {
-                y = value.x;
-                w = value.y;
-                x = value.z;
-                z = value.w;
-            }
+            get;
+            set;
         }
 
 
@@ -155,87 +101,33 @@ namespace Vectors.Interfaces
 
         public itvec4<T> zxyw
         {
-            get
-            {
-                return new tvec4<T>(z, x, y, w);
-            }
-            set
-            {
-                z = value.x;
-                x = value.y;
-                y = value.z;
-                w = value.w;
-            }
+            get;
+            set;
         }
         public itvec4<T> zyxw
         {
-            get
-            {
-                return new tvec4<T>(z, y, x, w);
-            }
-            set
-            {
-                z = value.x;
-                y = value.y;
-                x = value.z;
-                w = value.w;
-            }
+            get;
+            set;
         }
         public itvec4<T> zywx
         {
-            get
-            {
-                return new tvec4<T>(z, y, w, x);
-            }
-            set
-            {
-                z = value.x;
-                y = value.y;
-                w = value.z;
-                x = value.w;
-            }
+            get;
+            set;
         }
         public itvec4<T> zxwy
         {
-            get
-            {
-                return new tvec4<T>(z, x, w, y);
-            }
-            set
-            {
-                z = value.x;
-                x = value.y;
-                w = value.z;
-                y = value.w;
-            }
+            get;
+            set;
         }
         public itvec4<T> zwxy
         {
-            get
-            {
-                return new tvec4<T>(z, w, x, y);
-            }
-            set
-            {
-                z = value.x;
-                w = value.y;
-                x = value.z;
-                y = value.w;
-            }
+            get;
+            set;
         }
         public itvec4<T> zwyx
         {
-            get
-            {
-                return new tvec4<T>(z, w, y, x);
-            }
-            set
-            {
-                z = value.x;
-                w = value.y;
-                y = value.z;
-                x = value.w;
-            }
+            get;
+            set;
         }
 
 
@@ -244,99 +136,34 @@ namespace Vectors.Interfaces
 
         public itvec4<T> wxyz
         {
-            get
-            {
-                return new tvec4<T>(w, x, y, z);
-            }
-            set
-            {
-                w = value.x;
-                x = value.y;
-                y = value.z;
-                z = value.w;
-            }
+            get;
+            set;
         }
 
         public itvec4<T> wxzy
         {
-            get
-            {
-                return new tvec4<T>(w, x, z, y);
-            }
-            set
-            {
-                w = value.x;
-                x = value.y;
-                z = value.z;
-                y = value.w;
-            }
+            get;
+            set;
         }
         public itvec4<T> wyxz
         {
-            get
-            {
-                return new tvec4<T>(w, y, x, z);
-            }
-            set
-            {
-                w = value.x;
-                y = value.y;
-                x = value.z;
-                z = value.w;
-            }
+            get;
+            set;
         }
         public itvec4<T> wyzx
         {
-            get
-            {
-                return new tvec4<T>(w, y, z, x);
-            }
-            set
-            {
-                w = value.x;
-                y = value.y;
-                z = value.z;
-                x = value.w;
-            }
+            get;
+            set;
         }
         public itvec4<T> wzxy
         {
-            get
-            {
-                return new tvec4<T>(w, z, x, y);
-            }
-            set
-            {
-                w = value.x;
-                z = value.y;
-                x = value.z;
-                y = value.w;
-            }
+            get;
+            set;
         }
         public itvec4<T> wzyx
         {
-            get
-            {
-                return new tvec4<T>(w, z, y, x);
-            }
-            set
-            {
-                w = value.x;
-                z = value.y;
-                y = value.z;
-                x = value.w;
-            }
-        }
-
-
-
-        ///////////////////////////
-        //      Conversions      //
-        ///////////////////////////
-
-        public itvec4<itvec4<double>> fromDMat3(idmat4 input)
-        {
-            throw new NotImplementedException();
+            get;
+            set;
         }
 
 
