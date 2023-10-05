8976d5e90b9483a6a5461cea60a3ae30b450985f
Lewis Edwards <lewis@9thcircle.net>
2023-10-05 23:11:59 +1100

    While the granularity of doubles is tighter, changing the equality behaviour between data types is surprising and non-obvious

--- Vectors/Vectors.Core/scalar.cs
+++ Vectors/Vectors.Core/scalar.cs
@@ -8,20 +8,19 @@ namespace Vectors.Core
 {
     public class scalar
     {
+        const float REALLY_SMALL_VALUE = 0.000000001f;    //using float.epsilon is not better - floating point granularity is nonlinear
 
         //Yes: this sucks. 
 #if FLOATS_ENABLED
-        const float REALLY_SMALL_VALUE = 0.000000001f;    //using float.epsilon is not better - floating point granularity is nonlinear
         public static bool isClose(float x, float y)
         {
             return Math.Abs(x - y) <= REALLY_SMALL_VALUE;
         }
 #endif
 #if DOUBLES_ENABLED
-        const double EVEN_SMALLER_VALUE = 0.000000000000001;
         public static bool isClose(double x, double y)
         {
-            return Math.Abs(x - y) <= EVEN_SMALLER_VALUE;
+            return Math.Abs(x - y) <= REALLY_SMALL_VALUE;
         }
 #endif
     }
