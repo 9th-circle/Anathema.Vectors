c30788e734608611039860a3fe6b6870d0a41f3e
Lewis Edwards <lewis@9thcircle.net>
2023-10-08 22:19:56 +1100

    Added colour swizzles to fvec3

--- Vectors/Anathema.Vectors.Core/fvec3.cs
+++ Vectors/Anathema.Vectors.Core/fvec3.cs
@@ -191,6 +191,101 @@ namespace Anathema.Vectors.Core
         }
 #endif
 
+#if COLOURS_ENABLED
+        public float b
+        {
+            get
+            {
+                return z;
+            }
+            set
+            {
+                z = value;
+            }
+        }
+        public fvec3 rgb
+        {
+            get
+            {
+                return new fvec3(x, y, z);
+            }
+            set
+            {
+                x = value.x;
+                y = value.y;
+                z = value.z;
+            }
+        }
+        public fvec3 rbg
+        {
+            get
+            {
+                return new fvec3(x, z, y);
+            }
+            set
+            {
+                x = value.x;
+                z = value.y;
+                y = value.z;
+            }
+        }
+        public fvec3 grb
+        {
+            get
+            {
+                return new fvec3(y, x, z);
+            }
+            set
+            {
+                y = value.x;
+                x = value.y;
+                z = value.z;
+            }
+        }
+        public fvec3 gbr
+        {
+            get
+            {
+                return new fvec3(y, z, x);
+            }
+            set
+            {
+                y = value.x;
+                z = value.y;
+                x = value.z;
+            }
+        }
+        public fvec3 bgr
+        {
+            get
+            {
+                return new fvec3(z, y, x);
+            }
+            set
+            {
+                z = value.x;
+                y = value.y;
+                x = value.z;
+            }
+        }
+        public fvec3 brg
+        {
+            get
+            {
+                return new fvec3(z, x, y);
+            }
+            set
+            {
+                z = value.x;
+                x = value.y;
+                y = value.z;
+            }
+        }
+
+
+
+#endif
+
 
         ///////////////////////////
         //       Operators       //
