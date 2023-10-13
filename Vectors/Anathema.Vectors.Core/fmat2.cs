92e9c5a22d30e78f7c57f06852cef8f8e28835b1
Lewis Edwards <lewis@9thcircle.net>
2023-10-13 23:37:25 +1100

    Split out fmat3

--- Vectors/Anathema.Vectors.Core/fmat2.cs
+++ Vectors/Anathema.Vectors.Core/fmat2.cs
@@ -25,59 +25,6 @@ namespace Anathema.Vectors.Core
         {
             this.data = data;
         }
-        ///////////////////////////
-        //      Generators       //
-        ///////////////////////////
-
-
-        ///////////////////////////
-        //      Conversions      //
-        ///////////////////////////
-
-
-        ///////////////////////////
-        //      Transforms       //
-        ///////////////////////////
-
-        public fmat2 transposed
-        {
-            get
-            {
-                fmat2 output = new fmat2();
-                for (int x = 0; x < rowCount; x++)
-                    for (int y = 0; y < columnCount; y++)
-                        output.setValue(x, y, getValue(y, x));
-                return output;
-            }
-        }
-
-        ///////////////////////////
-        //       Mutators        //
-        ///////////////////////////
-
-        public virtual void transpose()
-        {
-            assign(transposed);
-        }
-
-        public void assign(fmat2 other)
-        {
-            for (int i = 0; i < other.data.Length && i < data.Length; i++)
-                data[i] = other.data[i];
-        }
-
-        ///////////////////////////
-        //       Operators       //
-        ///////////////////////////
-
-
-        public float this[int i] { get { return data[i]; } set { data[i] = value; } }
-        public float this[int x, int y] { get { return getValue(x,y); } set { setValue(x,y,value); } }
-
-        public float[] ToArray()
-        {
-            return data;
-        }
     }
 }
 #endif
---
+++ Vectors/Anathema.Vectors.Core/fmat3.operators.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Anathema.Vectors.Core
+{
+    public partial class fmat3
+    {
+        public override void transpose()
+        {
+            assign(transposed);
+        }
+
+        public void assign(fmat3 other)
+        {
+            for (int i = 0; i < other.data.Length && i < data.Length; i++)
+                data[i] = other.data[i];
+        }
+
+
+        public new fmat3 transposed
+        {
+            get
+            {
+                fmat3 output = new fmat3();
+                for (int x = 0; x < rowCount; x++)
+                    for (int y = 0; y < columnCount; y++)
+                        output.setValue(x, y, getValue(y, x));
+                return output;
+            }
+        }
+    }
+}
