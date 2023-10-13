b17a7af624e071fef483a190fff364f82b7aad71
Lewis Edwards <lewis@9thcircle.net>
2023-10-13 23:24:41 +1100

    Finished splitting dvec4

---
+++ Vectors/Anathema.Vectors.Core/dvec4.derivatives.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Anathema.Vectors.Core
+{
+    public partial class dvec4
+    {
+        public dvec3 scaledToDVec3()
+        {
+            return new dvec3(x / w, y / w, z / w);
+        }
+
+        public override double length
+        {
+            get
+            {
+                return Math.Sqrt((x * x) + (y * y) + (z * z) + (w * w));
+            }
+        }
+        public new dvec4 normalised
+        {
+            get
+            {
+                return this / length;
+            }
+        }
+    }
+}
