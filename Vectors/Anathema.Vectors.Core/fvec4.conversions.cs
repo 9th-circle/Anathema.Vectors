c57664a4e6d52cef70937477e2a1e1f53e666911
Lewis Edwards <lewis@9thcircle.net>
2023-10-13 23:11:58 +1100

    Split out fvec4

---
+++ Vectors/Anathema.Vectors.Core/fvec4.conversions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Anathema.Vectors.Core
+{
+    public partial class fvec4
+    {
+
+#if CONVERSIONS_ENABLED
+        public fvec4(fvec3 xyz, float w)
+        {
+            x = xyz.x;
+            y = xyz.y;
+            z = xyz.z;
+            this.w = w;
+        }
+        public fvec4(float x, fvec3 yzw)
+        {
+            this.x = x;
+            y = yzw.x;
+            z = yzw.y;
+            w = yzw.z;
+        }
+        public fvec4(fvec2 xy, fvec2 zw)
+        {
+            x = xy.x;
+            y = xy.y;
+            z = zw.x;
+            w = zw.y;
+        }
+        public fvec4(float x, float y, fvec2 zw)
+        {
+            this.x = x;
+            this.y = y;
+            z = zw.x;
+            w = zw.y;
+        }
+        public fvec4(float x, fvec2 yz, float w)
+        {
+            this.x = x;
+            y = yz.x;
+            z = yz.y;
+            this.w = w;
+        }
+        public fvec4(fvec2 xy, float z, float w)
+        {
+            x = xy.x;
+            y = xy.y;
+            this.z = z;
+            this.w = w;
+        }
+        public fvec4(dvec4 xyzw)
+        {
+            x = (float)xyzw.x;
+            y = (float)xyzw.y;
+            z = (float)xyzw.z;
+            w = (float)xyzw.w;
+        }
+
+
+#if TEMPLATES_ENABLED
+        public tvec4<float> toTVec4()
+        {
+            return new tvec4<float>(x, y, z, w);
+        }
+#endif
+#endif
+    }
+}
---
+++ Vectors/Anathema.Vectors.Core/fvec4.derivations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Anathema.Vectors.Core
+{
+    public partial class fvec4
+    {
+        public fvec3 scaledToFVec3()
+        {
+            return new fvec3(x / w, y / w, z / w);
+        }
+
+        public override float length
+        {
+            get
+            {
+                return (float)Math.Sqrt((x * x) + (y * y) + (z * z) + (w * w));
+            }
+        }
+        public new fvec4 normalised
+        {
+            get
+            {
+                return this / length;
+            }
+        }
+    }
+}
