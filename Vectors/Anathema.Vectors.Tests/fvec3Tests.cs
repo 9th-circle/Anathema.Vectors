ae1fb3f78af4cbc28aafa9f02decd2dabb0a5b1f
Lewis Edwards <lewis@9thcircle.net>
2023-10-14 11:47:20 +1100

    Started on fvec3 tests

---
+++ Vectors/Anathema.Vectors.Tests/fvec3Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Anathema.Vectors.Core;
+
+namespace Anathema.Vectors.Tests
+{
+    public class fvec3Tests
+    {
+        const float REALLY_SMALL_VALUE = 0.00005f;
+
+
+        [Theory]
+        [InlineData(new object[] { 1, 2, 3 })]
+        [InlineData(new object[] { 300000000, 2, 1.5 })]
+        [InlineData(new object[] { -1, 2.2342, 0.00001f })]
+        public void indices(float x, float y, float z)
+        {
+            fvec3 a = new fvec3(x, y, z);
+            Assert.Equal(a[0], x);
+            Assert.Equal(a[1], y);
+            Assert.Equal(a[1], y);
+        }
+    }
+}
