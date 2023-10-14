d0b0b2423c614e72100f528b2038aff60f4c4a6f
Lewis Edwards <lewis@9thcircle.net>
2023-10-14 12:29:27 +1100

    Implemented fvec3 arbitrary normalisation test

--- Vectors/Anathema.Vectors.Tests/fvec3Tests.cs
+++ Vectors/Anathema.Vectors.Tests/fvec3Tests.cs
@@ -24,5 +24,29 @@ namespace Anathema.Vectors.Tests
             Assert.Equal(a[1], y);
             Assert.Equal(a[1], y);
         }
+
+
+
+        [Theory]
+        [InlineData(new object[] { 1, 2, 3 })]
+        [InlineData(new object[] { 5.2f, 10.00001f, 12.345346356f })]
+        [InlineData(new object[] { -37, 0, 11 })]
+        [InlineData(new object[] { 3, 4, -4 })]
+        [InlineData(new object[] { 15.23f, 20.99999999999f, 100000000000.0f })]
+        [InlineData(new object[] { 2, -5, 0.1f })]
+        public void arbitaryNormalisation(float x, float y, float z)
+        {
+            fvec3 original = new fvec3(x, y, z);
+            fvec3 normalised = original.normalised;
+            fvec3 reconstructed = normalised * original.length;
+
+            Assert.True(Math.Abs(reconstructed.x - original.x) < REALLY_SMALL_VALUE);
+            Assert.True(Math.Abs(reconstructed.y - original.y) < REALLY_SMALL_VALUE);
+        }
+
+
+
+
+
     }
 }
