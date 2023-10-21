5e799f04bed052bb0041fa41fb7a02ba426ac7b7
Lewis Edwards <lewis@9thcircle.net>
2023-10-21 22:27:21 +1100

    Implemented full dmat2 test suite

---
+++ Vectors/Anathema.Vectors.Tests/DoubleMatrices/dmat2Constructors.cs
@@ -0,0 +1,85 @@
+﻿using Anathema.Vectors.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Anathema.Vectors.Tests.DoubleMatrices
+{
+    public class dmat2Constructors
+    {
+        [Fact]
+        public void defaultConstructor()
+        {
+            dmat2 def = new dmat2();
+            Assert.Equal(0, def[0]);
+            Assert.Equal(0, def[1]);
+            Assert.Equal(0, def[2]);
+            Assert.Equal(0, def[3]);
+        }
+        [Fact]
+        public void outOfRangeException()
+        {
+            dmat2 def = new dmat2();
+            Assert.Throws<IndexOutOfRangeException>(delegate () { def.setValue(1, 2, 0); });
+            Assert.Equal(0, def.getValue(2, 1));
+        }
+        [Fact]
+        public void readColumnOrder()
+        {
+            dmat2 def = new dmat2(new double[] { 1, 2, 3, 4 });
+
+            Assert.Equal(1, def.getValue(0, 0));
+            Assert.Equal(2, def.getValue(1, 0));
+            Assert.Equal(3, def.getValue(0, 1));
+            Assert.Equal(4, def.getValue(1, 1));
+
+            Assert.Equal(1, def[0, 0]);
+            Assert.Equal(2, def[1, 0]);
+            Assert.Equal(3, def[0, 1]);
+            Assert.Equal(4, def[1, 1]);
+
+            Assert.Equal(1, def[0]);
+            Assert.Equal(2, def[1]);
+            Assert.Equal(3, def[2]);
+            Assert.Equal(4, def[3]);
+        }
+        [Fact]
+        public void writeColumnOrder()
+        {
+            dmat2 def = new dmat2();
+
+            def.setValue(0, 0, 1);
+            def.setValue(1, 0, 2);
+            def.setValue(0, 1, 3);
+            def.setValue(1, 1, 4);
+
+            Assert.Equal(1, def.getValue(0, 0));
+            Assert.Equal(2, def.getValue(1, 0));
+            Assert.Equal(3, def.getValue(0, 1));
+            Assert.Equal(4, def.getValue(1, 1));
+
+            def[0] = 10;
+            def[1] = 11;
+            def[2] = 12;
+            def[3] = 13;
+
+            Assert.Equal(10, def[0, 0]);
+            Assert.Equal(11, def[1, 0]);
+            Assert.Equal(12, def[0, 1]);
+            Assert.Equal(13, def[1, 1]);
+
+            def[0, 0] = 9;
+            def[1, 0] = 8;
+            def[0, 1] = 7;
+            def[1, 1] = 6;
+
+            Assert.Equal(9, def[0]);
+            Assert.Equal(8, def[1]);
+            Assert.Equal(7, def[2]);
+            Assert.Equal(6, def[3]);
+        }
+    }
+}
---
+++ Vectors/Anathema.Vectors.Tests/DoubleMatrices/dmat2Generators.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Anathema.Vectors.Core;
+using Xunit;
+
+namespace Anathema.Vectors.Tests.DoubleMatrices
+{
+    public class dmat2Generators
+    {
+        [Fact]
+        public void identity()
+        {
+            dmat2 def = dmat2.identity();
+            Assert.Equal(1, def[0]);
+            Assert.Equal(0, def[1]);
+            Assert.Equal(0, def[2]);
+            Assert.Equal(1, def[3]);
+        }
+        [Fact]
+        public void fromNested()
+        {
+            tvec2<tvec2<double>> nested = new tvec2<tvec2<double>>();
+            nested.x = new tvec2<double>();
+            nested.y = new tvec2<double>();
+            nested.x.x = 1;
+            nested.x.y = 2;
+            nested.y.x = 3;
+            nested.y.y = 4;
+            dmat2 output = dmat2.fromNestedVector(nested);
+            Assert.Equal(1, output[0]);
+            Assert.Equal(3, output[1]);
+            Assert.Equal(2, output[2]);
+            Assert.Equal(4, output[3]);
+        }
+        [Fact]
+        public void array()
+        {
+            double[] array = dmat2.identity().ToArray();
+            Assert.Equal(1, array[0]);
+            Assert.Equal(0, array[1]);
+            Assert.Equal(0, array[2]);
+            Assert.Equal(1, array[3]);
+        }
+        [Fact]
+        public void transposeImperative()
+        {
+            dmat2 def = new dmat2(new double[] { 1, 2, 3, 4 });
+            def.transpose();
+            Assert.Equal(1, def[0]);
+            Assert.Equal(3, def[1]);
+            Assert.Equal(2, def[2]);
+            Assert.Equal(4, def[3]);
+        }
+        [Fact]
+        public void copy()
+        {
+            dmat2 original = new dmat2(new double[] { 1, 2, 3, 4 });
+            dmat2 copy = new dmat2(original);
+            Assert.Equal(1, copy[0]);
+            Assert.Equal(2, copy[1]);
+            Assert.Equal(3, copy[2]);
+            Assert.Equal(4, copy[3]);
+        }
+        [Fact]
+        public void scaleUniform()
+        {
+            dmat2 def = dmat2.scale(5.0f);
+            Assert.Equal(5, def[0]);
+            Assert.Equal(0, def[1]);
+            Assert.Equal(0, def[2]);
+            Assert.Equal(5, def[3]);
+        }
+        [Fact]
+        public void scaleBasis()
+        {
+            dmat2 def = dmat2.scale(new dvec2(2, 3));
+            Assert.Equal(2, def[0]);
+            Assert.Equal(0, def[1]);
+            Assert.Equal(0, def[2]);
+            Assert.Equal(3, def[3]);
+        }
+
+        [Theory]
+        [InlineData(0, 1, 0, 0, 1)]
+        [InlineData(90, 0, 1, -1, 0)]
+        [InlineData(180, -1, 0, 0, -1)]
+        [InlineData(270, 0, -1, 1, 0)]
+        public void rotationDegrees(double angle, double expected0, double expected1, double expected2, double expected3)
+        {
+            dmat2 matrix = dmat2.rotateDegrees(angle);
+            Assert.True(Math.Abs(expected0 - matrix[0]) < scalar.floatComparisonTolerance);
+            Assert.True(Math.Abs(expected1 - matrix[1]) < scalar.floatComparisonTolerance);
+            Assert.True(Math.Abs(expected2 - matrix[2]) < scalar.floatComparisonTolerance);
+            Assert.True(Math.Abs(expected3 - matrix[3]) < scalar.floatComparisonTolerance);
+        }
+        [Theory]
+        [InlineData(0, 1, 0, 0, 1)]
+        [InlineData(Math.PI / 2, 0, 1, -1, 0)]
+        [InlineData(Math.PI, -1, 0, 0, -1)]
+        [InlineData(Math.PI * 3.0f / 2, 0, -1, 1, 0)]
+        public void rotationRadians(double angle, double expected0, double expected1, double expected2, double expected3)
+        {
+            dmat2 matrix = dmat2.rotateRadians(angle);
+            Assert.True(Math.Abs(expected0 - matrix[0]) < scalar.floatComparisonTolerance);
+            Assert.True(Math.Abs(expected1 - matrix[1]) < scalar.floatComparisonTolerance);
+            Assert.True(Math.Abs(expected2 - matrix[2]) < scalar.floatComparisonTolerance);
+            Assert.True(Math.Abs(expected3 - matrix[3]) < scalar.floatComparisonTolerance);
+        }
+    }
+}
