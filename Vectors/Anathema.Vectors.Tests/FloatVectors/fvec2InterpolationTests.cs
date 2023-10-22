d0f52a0d9225e0d9dba5c4ca6113b10324f521b3
Lewis Edwards <lewis@9thcircle.net>
2023-10-22 14:11:02 +1100

    fvec2 quadratic bezier to test

--- Vectors/Anathema.Vectors.Tests/FloatVectors/fvec2InterpolationTests.cs
+++ Vectors/Anathema.Vectors.Tests/FloatVectors/fvec2InterpolationTests.cs
@@ -6,6 +6,23 @@ namespace Anathema.Vectors.Tests.FloatVectors
 {
     public class fvec2InterpolationTests
     {
+
+        [Fact]
+        public void degenerateQuadraticBezierTo()
+        {
+            fvec2 a = new fvec2(10, 20);
+            fvec2 b = new fvec2(15, 25);
+            fvec2 c = new fvec2(20, 30);
+
+            for (int i = 0; i < 100; i++)
+            {
+                float f = i / 100.0f;
+                float expectedX = (i / 10.0f) + 10;
+                float expectedY = (i / 10.0f) + 20;
+                Assert.True(Math.Abs(expectedX - a.quadraticBezierInterpolateTo(b, c, f).x) < scalar.floatComparisonTolerance);
+                Assert.True(Math.Abs(expectedY - a.quadraticBezierInterpolateTo(b, c, f).y) < scalar.floatComparisonTolerance);
+            }
+        }
         [Fact]
         public void degenerateQuadraticBezier()
         {
@@ -20,7 +37,7 @@ namespace Anathema.Vectors.Tests.FloatVectors
                 float expectedY = (i / 10.0f) + 20;
                 Assert.True(Math.Abs(expectedX - fvec2.quadraticBezierInterpolate(a, b, c, f).x) < scalar.floatComparisonTolerance);
                 Assert.True(Math.Abs(expectedY - fvec2.quadraticBezierInterpolate(a, b, c, f).y) < scalar.floatComparisonTolerance);
-        }
+            }
         }
         [Fact]
         public void nonDegenerateQuadraticBezier()
