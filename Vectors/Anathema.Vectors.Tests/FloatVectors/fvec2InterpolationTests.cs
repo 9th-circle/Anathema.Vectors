01e2dc7df449adde07a4a932e480aeb69ef39847
Lewis Edwards <lewis@9thcircle.net>
2023-10-22 14:13:47 +1100

    Linear interpolation fvec2 test

--- Vectors/Anathema.Vectors.Tests/FloatVectors/fvec2InterpolationTests.cs
+++ Vectors/Anathema.Vectors.Tests/FloatVectors/fvec2InterpolationTests.cs
@@ -8,6 +8,21 @@ namespace Anathema.Vectors.Tests.FloatVectors
     {
 
         [Fact]
+        public void linearInterpolateTo()
+        {
+            fvec2 a = new fvec2(10, 20);
+            fvec2 c = new fvec2(20, 30);
+
+            for (int i = 0; i < 100; i++)
+            {
+                float f = i / 100.0f;
+                float expectedX = (i / 10.0f) + 10;
+                float expectedY = (i / 10.0f) + 20;
+                Assert.True(Math.Abs(expectedX - a.linearInterpolateTo(c, f).x) < scalar.floatComparisonTolerance);
+                Assert.True(Math.Abs(expectedY - a.linearInterpolateTo(c, f).y) < scalar.floatComparisonTolerance);
+            }
+        }
+        [Fact]
         public void degenerateQuadraticBezierTo()
         {
             fvec2 a = new fvec2(10, 20);
