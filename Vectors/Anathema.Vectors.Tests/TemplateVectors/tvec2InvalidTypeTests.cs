f50d6d44250b2a2a4d5e886c8b8068d948e6fd8b
Lewis Edwards <lewis@9thcircle.net>
2023-10-22 17:23:34 +1100

    Assert appropriate throws for tvec2<> with an invalid T (ie string)

---
+++ Vectors/Anathema.Vectors.Tests/TemplateVectors/tvec2InvalidTypeTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Anathema.Vectors.Core;
+using Xunit;
+
+namespace Anathema.Vectors.Tests.TemplateVectors
+{
+    public class tvec2InvalidTypeTests
+    {
+        [Fact]
+        public void invalidOperators()
+        {
+            tvec2<string> a = new tvec2<string>("a", "b");
+            tvec2<string> b = new tvec2<string>("c", "d");
+
+            //Assert.Throws<TypeLoadException>(delegate () { var v = -a; });
+
+            Assert.Throws<TypeLoadException>(delegate () { var v = a + b; });
+            Assert.Throws<TypeLoadException>(delegate () { var v = a - b; });
+            Assert.Throws<TypeLoadException>(delegate () { var v = a / b; });
+            Assert.Throws<TypeLoadException>(delegate () { var v = a * b; });
+
+            Assert.Throws<TypeLoadException>(delegate () { var v = a + "z"; });
+            Assert.Throws<TypeLoadException>(delegate () { var v = a - "z"; });
+            Assert.Throws<TypeLoadException>(delegate () { var v = a / "z"; });
+            Assert.Throws<TypeLoadException>(delegate () { var v = a * "z"; });
+        }
+
+    }
+}
+
