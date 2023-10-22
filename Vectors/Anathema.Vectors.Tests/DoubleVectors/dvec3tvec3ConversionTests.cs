ee0a9a8c9e1e6ef0cb5ad22ad288ca96dc8239d3
Lewis Edwards <lewis@9thcircle.net>
2023-10-22 18:05:27 +1100

    test for dvec3 to template

---
+++ Vectors/Anathema.Vectors.Tests/DoubleVectors/dvec3tvec3ConversionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Anathema.Vectors.Core;
+using Xunit;
+
+namespace Anathema.Vectors.Tests.DoubleVectors
+{
+    public class dvec3tvec3ConversionTests
+    {
+        [Fact]
+        public void dvec3toTemplate()
+        {
+            dvec3 a = new dvec3(1, 2, 3);
+            tvec3<double> b = a.toTVec3();
+
+            Assert.Equal(1, b.x);
+            Assert.Equal(2, b.y);
+            Assert.Equal(3, b.z);
+        }
+    }
+}
