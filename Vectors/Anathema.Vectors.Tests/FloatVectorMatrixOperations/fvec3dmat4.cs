7f15375c2dde49f329df11d188dc40ef2d3fde4f
Lewis Edwards <lewis@9thcircle.net>
2023-10-22 14:50:50 +1100

    Unit tests for fvec3-matrix operations

---
+++ Vectors/Anathema.Vectors.Tests/FloatVectorMatrixOperations/fvec3dmat4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Anathema.Vectors.Core;
+using Xunit;
+
+namespace Anathema.Vectors.Tests.FloatVectorMatrixOperations
+{
+    public class fvec3dmat4
+    {
+        [Fact]
+        public void identityMultiply()
+        {
+            fvec3 a = new fvec3(1, 2, 3);
+            dmat4 m = dmat4.identity();
+
+            dvec3 b = m * a;
+            dvec3 c = a * m;
+
+            Assert.Equal(1, b.x);
+            Assert.Equal(2, b.y);
+            Assert.Equal(3, b.z);
+
+            Assert.Equal(1, c.x);
+            Assert.Equal(2, c.y);
+            Assert.Equal(3, c.z);
+        }
+    }
+}
