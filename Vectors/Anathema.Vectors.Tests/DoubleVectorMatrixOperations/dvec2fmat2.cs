776140a8047b47791ca84cffd66535ebff999431
Lewis Edwards <lewis@9thcircle.net>
2023-10-22 15:47:59 +1100

    dvec2 - fmat2 tests

---
+++ Vectors/Anathema.Vectors.Tests/DoubleVectorMatrixOperations/dvec2fmat2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Anathema.Vectors.Core;
+using Xunit;
+
+namespace Anathema.Vectors.Tests.FloatVectorMatrixOperations
+{
+    public class dvec2fmat2
+    {
+        [Theory]
+        [InlineData(0, 5, 0, 5, 0)]
+        [InlineData(90, 1, 0, 0, -1)]
+        [InlineData(180, 10, 0, -10, 0)]
+        public void rotateDegrees(double angle, double startX, double startY, double endX, double endY)
+        {
+            dvec2 start = new dvec2(startX, startY);
+            fmat2 rotation = fmat2.rotateDegrees((float)angle);
+            dvec2 end = rotation * start;
+            dvec2 reverse = end * rotation;
+
+            Assert.True(Math.Abs(end.x - endX) < scalar.floatComparisonTolerance);
+            Assert.True(Math.Abs(end.y - endY) < scalar.floatComparisonTolerance);
+            Assert.True(Math.Abs(start.x - reverse.x) < scalar.floatComparisonTolerance);
+            Assert.True(Math.Abs(start.y - reverse.y) < scalar.floatComparisonTolerance);
+        }
+        [Theory]
+        [InlineData(0, 5, 0, 5, 0)]
+        [InlineData(90, 1, 0, 0, -1)]
+        [InlineData(180, 10, 0, -10, 0)]
+        public void rotateDegreesfmat(double angle, double startX, double startY, double endX, double endY)
+        {
+            dvec2 start = new dvec2(startX, startY);
+            fmat2 rotation = fmat2.rotateDegrees((float)angle);
+            dvec2 end = rotation * start;
+            dvec2 reverse = end * rotation;
+
+            Assert.True(Math.Abs(end.x - endX) < scalar.floatComparisonTolerance);
+            Assert.True(Math.Abs(end.y - endY) < scalar.floatComparisonTolerance);
+            Assert.True(Math.Abs(start.x - reverse.x) < scalar.floatComparisonTolerance);
+            Assert.True(Math.Abs(start.y - reverse.y) < scalar.floatComparisonTolerance);
+        }
+    }
+}
