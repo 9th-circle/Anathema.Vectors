caa93aed90d0ba7d189e3c8dcc11e39e8295c3e8
Lewis Edwards <lewis@9thcircle.net>
2023-10-22 14:03:05 +1100

    fvec3->tvec3 conversion test

---
+++ Vectors/Anathema.Vectors.Tests/FloatVectors/fvec3tvec3ConversionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Anathema.Vectors.Core;
+using Xunit;
+
+namespace Anathema.Vectors.Tests.FloatVectors
+{
+    public class fvec3tvec3ConversionTests
+    {
+        [Fact]
+        public void basicTemplateConversion()
+        {
+            fvec3 a = new fvec3(10, 20, 30);
+            tvec3<float> t = a.toTVec3();
+
+            Assert.Equal(10, t.x);
+            Assert.Equal(20, t.y);
+            Assert.Equal(30, t.z);
+        }
+    }
+}
