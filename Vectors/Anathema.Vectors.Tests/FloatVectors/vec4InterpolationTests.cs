124a18dc9c9d011843210a12d2ffa89059b51e50
Lewis Edwards <lewis@9thcircle.net>
2023-10-24 13:53:03 +1100

    Translated across interpolation tests from vec3->(d)vec4

---
+++ Vectors/Anathema.Vectors.Tests/FloatVectors/vec4InterpolationTests.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Anathema.Vectors.Core;
+using Xunit;
+
+namespace Anathema.Vectors.Tests.FloatVectors
+{
+    public class vec4InterpolationTests
+    {
+        [Fact]
+        public void linearInterpolateTo()
+        {
+            vec4 a = new vec4(10, 20, 30, 40);
+            vec4 c = new vec4(20, 30, 40, 50);
+
+            for (int i = 0; i < 100; i++)
+            {
+                float f = i / 100.0f;
+                float expectedX = (i / 10.0f) + 10;
+                float expectedY = (i / 10.0f) + 20;
+                float expectedZ = (i / 10.0f) + 30;
+                Assert.True(Math.Abs(expectedX - a.linearInterpolateTo(c, f).x) < scalar.floatComparisonTolerance);
+                Assert.True(Math.Abs(expectedY - a.linearInterpolateTo(c, f).y) < scalar.floatComparisonTolerance);
+                Assert.True(Math.Abs(expectedZ - a.linearInterpolateTo(c, f).z) < scalar.floatComparisonTolerance);
+            }
+        }
+        [Fact]
+        public void degenerateQuadraticBezierTo()
+        {
+            vec4 a = new vec4(10, 20, 30, 40);
+            vec4 b = new vec4(15, 25, 35, 45);
+            vec4 c = new vec4(20, 30, 40, 50);
+
+            for (int i = 0; i < 100; i++)
+            {
+                float f = i / 100.0f;
+                float expectedX = (i / 10.0f) + 10;
+                float expectedY = (i / 10.0f) + 20;
+                float expectedZ = (i / 10.0f) + 30;
+                Assert.True(Math.Abs(expectedX - a.quadraticBezierInterpolateTo(b, c, f).x) < scalar.floatComparisonTolerance);
+                Assert.True(Math.Abs(expectedY - a.quadraticBezierInterpolateTo(b, c, f).y) < scalar.floatComparisonTolerance);
+                Assert.True(Math.Abs(expectedZ - a.quadraticBezierInterpolateTo(b, c, f).z) < scalar.floatComparisonTolerance);
+
+            }
+        }
+        [Fact]
+        public void degenerateCubicBezier()
+        {
+            vec4 a = new vec4(10, 10, 10, 10);
+            vec4 b = new vec4(15, 15, 15, 15);
+            vec4 c = new vec4(20, 20, 20, 20);
+            vec4 d = new vec4(25, 25, 25, 25);
+
+            for (int i = 0; i < 100; i++)
+            {
+                float f = i / 100.0f;
+                Assert.True(Math.Abs(vec4.cubicBezierInterpolate(a, b, c, d, f).x -
+                                     vec4.cubicBezierInterpolate(a, b, c, d, f).y) < scalar.floatComparisonTolerance);
+                Assert.True(Math.Abs(vec4.cubicBezierInterpolate(a, b, c, d, f).x -
+                                     vec4.cubicBezierInterpolate(a, b, c, d, f).z) < scalar.floatComparisonTolerance);
+
+            }
+        }
+        [Fact]
+        public void nonDegenerateCubicBezier()
+        {
+            vec4 a = new vec4(10, 10, 10, 10);
+            vec4 b = new vec4(20, 10, 20, 20);
+            vec4 c = new vec4(10, 15, 10, 10);
+            vec4 d = new vec4(25, 25, 25, 25);
+
+            for (int i = 10; i < 90; i++)
+            {
+                float f = i / 100.0f;
+                Assert.True(Math.Abs(vec4.cubicBezierInterpolate(a, b, c, d, f).x -
+                                     vec4.cubicBezierInterpolate(a, b, c, d, f).y) > scalar.floatComparisonTolerance);
+            }
+        }
+        [Fact]
+        public void degenerateCubicBezierTo()
+        {
+            vec4 a = new vec4(10, 10, 10, 10);
+            vec4 b = new vec4(15, 15, 15, 15);
+            vec4 c = new vec4(20, 20, 20, 20);
+            vec4 d = new vec4(25, 25, 25, 25);
+
+            for (int i = 0; i < 100; i++)
+            {
+                float f = i / 100.0f;
+                Assert.True(Math.Abs(a.cubicBezierInterpolateTo(b, c, d, f).x -
+                                     a.cubicBezierInterpolateTo(b, c, d, f).y) < scalar.floatComparisonTolerance);
+                Assert.True(Math.Abs(a.cubicBezierInterpolateTo(b, c, d, f).x -
+                                     a.cubicBezierInterpolateTo(b, c, d, f).z) < scalar.floatComparisonTolerance);
+            }
+        }
+        [Fact]
+        public void degenerateQuadraticBezier()
+        {
+            vec4 a = new vec4(10, 20, 30, 40);
+            vec4 b = new vec4(15, 25, 35, 45);
+            vec4 c = new vec4(20, 30, 40, 50);
+
+            for (int i = 0; i < 100; i++)
+            {
+                float f = i / 100.0f;
+                float expectedX = (i / 10.0f) + 10;
+                float expectedY = (i / 10.0f) + 20;
+                float expectedZ = (i / 10.0f) + 30;
+                Assert.True(Math.Abs(expectedX - vec4.quadraticBezierInterpolate(a, b, c, f).x) < scalar.floatComparisonTolerance);
+                Assert.True(Math.Abs(expectedY - vec4.quadraticBezierInterpolate(a, b, c, f).y) < scalar.floatComparisonTolerance);
+                Assert.True(Math.Abs(expectedZ - vec4.quadraticBezierInterpolate(a, b, c, f).z) < scalar.floatComparisonTolerance);
+
+            }
+        }
+        [Fact]
+        public void nonDegenerateQuadraticBezier()
+        {
+            vec4 a = new vec4(10, 20, 30, 40);
+            vec4 b = new vec4(30, 40, 50, 60);
+            vec4 c = new vec4(20, 30, 40, 50);
+
+            for (int i = 10; i < 90; i++)
+            {
+                float f = i / 100.0f;
+                float expectedX = (i / 10.0f) + 10;
+                float expectedY = (i / 10.0f) + 20;
+                float expectedZ = (i / 10.0f) + 30;
+                Assert.True(Math.Abs(expectedX - vec4.quadraticBezierInterpolate(a, b, c, f).x) > scalar.floatComparisonTolerance);
+                Assert.True(Math.Abs(expectedY - vec4.quadraticBezierInterpolate(a, b, c, f).y) > scalar.floatComparisonTolerance);
+                Assert.True(Math.Abs(expectedZ - vec4.quadraticBezierInterpolate(a, b, c, f).z) > scalar.floatComparisonTolerance);
+            }
+        }
+    }
+}
