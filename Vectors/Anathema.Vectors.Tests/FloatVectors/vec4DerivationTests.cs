1020dd6e6d4b19738bd4f18e6e407365b740e290
Lewis Edwards <lewis@9thcircle.net>
2023-10-24 13:17:23 +1100

    vec4 length unit test

---
+++ Vectors/Anathema.Vectors.Tests/FloatVectors/vec4DerivationTests.cs
@@ -0,0 +1,15 @@
+﻿using Anathema.Vectors.Core;
+using Xunit;
+
+namespace Anathema.Vectors.Tests.FloatVectors
+{
+    public class vec4DerivationTests
+    {
+        [Fact]
+        public void length()
+        {
+            vec4 a = new vec4(0, 0, 0, 100);
+            Assert.Equal(100, a.length);
+        }
+    }
+}
