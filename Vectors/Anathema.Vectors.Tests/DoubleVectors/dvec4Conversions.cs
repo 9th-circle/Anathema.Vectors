7a95c3786a0c8276f058b41bc5f393202bc5f099
Lewis Edwards <lewis@9thcircle.net>
2023-10-24 12:30:07 +1100

    (d)vec4->tvec4 conversion tests

---
+++ Vectors/Anathema.Vectors.Tests/DoubleVectors/dvec4Conversions.cs
@@ -0,0 +1,25 @@
+﻿using Anathema.Vectors.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Anathema.Vectors.Tests.DoubleVectors
+{
+    public class dvec4Conversions
+    {
+        [Fact]
+        public void basicTemplateConversion()
+        {
+            dvec4 a = new dvec4(10, 20, 30, 40);
+            tvec4<double> t = a.toTVec4();
+
+            Assert.Equal(10, t.x);
+            Assert.Equal(20, t.y);
+            Assert.Equal(30, t.z);
+            Assert.Equal(40, t.w);
+        }
+    }
+}
---
+++ Vectors/Anathema.Vectors.Tests/FloatVectors/vec4Conversions.cs
@@ -0,0 +1,25 @@
+﻿using Anathema.Vectors.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Anathema.Vectors.Tests.FloatVectors
+{
+    public class vec4Conversions
+    {
+        [Fact]
+        public void basicTemplateConversion()
+        {
+            vec4 a = new vec4(10, 20, 30, 40);
+            tvec4<float> t = a.toTVec4();
+
+            Assert.Equal(10, t.x);
+            Assert.Equal(20, t.y);
+            Assert.Equal(30, t.z);
+            Assert.Equal(40, t.w);
+        }
+    }
+}
