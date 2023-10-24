e46699b5d1c51563400d13abb26edf909e691b11
Lewis Edwards <lewis@9thcircle.net>
2023-10-24 13:46:41 +1100

    Copied vec4 derivation tests to dvec4

---
+++ Vectors/Anathema.Vectors.Tests/DoubleVectors/dvec4DerivationTests.cs
@@ -0,0 +1,40 @@
+﻿using Anathema.Vectors.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Anathema.Vectors.Tests.DoubleVectors
+{
+    public class dddvec4DerivationTests
+    {
+        [Fact]
+        public void length()
+        {
+            dvec4 a = new dvec4(0, 0, 0, 100);
+            Assert.Equal(100, a.length);
+        }
+        [Fact]
+        public void normalise()
+        {
+            dvec4 a = new dvec4(0, 0, 0, 100).normalised;
+            Assert.Equal(1, a.length);
+            dvec4 b = new dvec4(0, -50, 0, 0);
+            b.normalise();
+            Assert.Equal(1, b.length);
+        }
+        [Fact]
+        public void dot()
+        {
+            dvec4 a = new dvec4(1, 1, 1, 1);
+            dvec4 b = new dvec4(1, 1, 1, 1);
+            Assert.Equal(1, a.dot(b));
+
+            dvec4 c = new dvec4(1, 1, 1, 5);
+            dvec4 d = new dvec4(1, 1, 1, 1);
+            Assert.Equal(5, a.dot(b));
+        }
+    }
+}
